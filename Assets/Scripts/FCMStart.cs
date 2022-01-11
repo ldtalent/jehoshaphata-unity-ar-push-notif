cac9e5688bc99b69002b19efb6ff214b0a2489b0
Jehoshaphat I. Abu <jehoshaphatia@outlook.com>
2022-01-12 00:08:32 +0100

    chore: Clean things up

--- Assets/Scripts/FCMStart.cs
+++ Assets/Scripts/FCMStart.cs
@@ -27,19 +27,19 @@ public class FCMStart : MonoBehaviour
     }
 
     // Start is called before the first frame update
-        public void Start()
-        {
-            Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
-            Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
-        }
+    public void Start()
+    {
+        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
 
-        public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
-        {
-            UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
-        }
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    }
 
-        public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
-        {
-            UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
-        }
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
 }
